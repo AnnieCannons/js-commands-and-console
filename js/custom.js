9334c1149535478557b44142f400e39a30adcd28
[Hailey G] <[haileydgrace@gmail.com]>
2023-08-01 13:54:55 -0400

    assignment

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+console.log("Hello, World!");
+console.log("Hailey!");
+console.log("2" + "2");
+console.log(2 + 2);
+console.log("2+2");
+/* alert("Hello, World!");
+alert("ERROR: PAGE NOT FOUND"); */
+
+let firstName = prompt ("What is your fist name?");
+console.log(firstName);
