19089068c0a3dba3285baee7a047e4e36d522d42
shan wolff <shanwolff182@gmail.com>
2023-08-08 16:44:00 -0700

    Hello world exercise complete

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+console.log("Hello World!")
+console.log("Shan Wolff")
+console.log(2+2)
+/*alert("Hello World!")
+alert("Do you wish to proceed")*/
+
+
+let firstName = prompt ("What is your first name?");
+console.log(firstName);
+
